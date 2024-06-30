eecef1fd9da47ad675bd0c8c2d825253669a6179
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-06-30 22:46:16 +0530

    React Code Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Fragment} from 'react';
 import './App.css';
+import MessageCard from "./components/MessageCard";
 
-function App() {
+let App = () => {
   return (
     <Fragment>
-      <div className="App">
-        <h2>Welcome to App Component</h2>
-        <img src='https://fastly.picsum.photos/id/18/2500/1667.jpg?hmac=JR0Z_jRs9rssQHZJ4b7xKF82kOj8-4Ackq75D_9Wmz8' alt='Image' width={500} height={300}></img>
-      </div>
-      <div className="App">
-        <h2>Welcome to App Component</h2>
-        <img src='https://fastly.picsum.photos/id/18/2500/1667.jpg?hmac=JR0Z_jRs9rssQHZJ4b7xKF82kOj8-4Ackq75D_9Wmz8' alt='Image' width={500} height={300}></img>
-      </div>
+      <nav className="navbar">
+        <span>Welcome to App Component</span>
+      </nav>
+      <MessageCard/>
     </Fragment>
     
   );
