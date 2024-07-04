f6ddd0f8170f7cadd6f38d0756141f79fbc3f6ee
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-07-04 19:41:48 +0530

    React Code Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Fragment} from 'react';
 import './App.css';
-import Employee from './components/Employee';
-import Student from './components/Student';
+
+
+
 
 
 let App = () => {
@@ -10,10 +11,7 @@ let App = () => {
       <nav className="navbar">
         <span>Welcome to App Component</span>
       </nav>
-      {/* <Employee name ="Raj" age={25}/>
-      <Employee name ="Kumar" age={30}/> */}
-      <Student name = "John" course ="B.Tech"/>
-      <Student name = "Laura" course ="MBBS"/>
+      
     </Fragment>
     
   );
