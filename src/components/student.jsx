87c48b5c6d140ed3348fbe148ab9d7484986ef9b
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-07-14 23:17:09 +0530

    State Management in Functional Component

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 let Student = () => {
+    let [state, setState] = useState({
+        students : [
+            {
+                name : 'John',
+                course : 'B.Tech'
+            },
+            {
+                name : 'Laura',
+                course : 'MBBS'
+            }
+        ]
+    });
+
+    let {students} = state; //Destructuring Concept in ES6
+
     return(
         <React.Fragment>
-            <h2>Welcome to Student</h2>
+            <div className="card">
+                <div className="card-body">
+                    <pre>{JSON.stringify(state)}</pre>
+                    <span>Name : {state.students[0].name}</span><br/>
+                    <span>Course : {state.students[0].course}</span>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="card-body">
+                    <pre>{JSON.stringify(state)}</pre>
+                    <span>Name : {students[1].name}</span><br/>
+                    <span>Course : {students[1].course}</span>
+                </div>
+            </div>
         </React.Fragment>
     )
 };
