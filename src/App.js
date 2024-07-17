0946a273c4b710463eeb33db96524789918fb6cc
Srirajkumar <ksrirajk@email.com>
2024-07-17 19:03:20 +0530

    React Code Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Fragment} from 'react';
 import './App.css';
-import CountryCard from './components/CountryCard';
 
 
 
@@ -11,7 +10,6 @@ let App = () => {
       <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
         <a href="/" className='navbar-brand'>React with Bootstrap Styles</a>
       </nav>
-      <CountryCard/>
       
     </Fragment>
     
