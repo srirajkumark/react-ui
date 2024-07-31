51b0c583684e1f653f6aaf59e0d348a473a0af81
Srirajkumar <ksrirajk@email.com>
2024-07-31 19:59:06 +0530

    Life Cycle Methods

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment} from 'react';
 import './App.css';
-import ContactApp from './components/ContactApp';
+
 
 
 
@@ -8,9 +8,8 @@ let App = () => {
   return (
     <Fragment>
       <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
-        <a href="/" className='navbar-brand'>React with Contact App</a>
+        <a href="/" className='navbar-brand'>React with LifeCycle Methods</a>
       </nav>
-      <ContactApp/>
     </Fragment>
     
   );
