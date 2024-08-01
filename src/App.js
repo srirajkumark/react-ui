5116046dfcd2967c146a9cc58205bd4767ea701a
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-08-01 14:37:46 +0530

    Server Connection Setup - Axios Installation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Fragment} from 'react';
 import './App.css';
-import DigitalWatch from './components/DigitalWatch';
-
-
 
 
 let App = () => {
   return (
     <Fragment>
       <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
-        <a href="/" className='navbar-brand'>React with LifeCycle Methods</a>
+        <a href="/" className='navbar-brand'>React with Server Connection</a>
       </nav>
-      <DigitalWatch/>
     </Fragment>
     
   );
