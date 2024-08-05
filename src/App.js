7524ffb33a8cd64f8069aa617c8afabbc93f344d
Srirajkumar <ksrirajk@email.com>
2024-08-05 19:40:43 +0530

    Contact App using Http Axios Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Fragment} from 'react';
 import './App.css';
-import UserList from './components/UserList';
 
 
 let App = () => {
   return (
     <Fragment>
       <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
-        <a href="/" className='navbar-brand'>React with Server Connection</a>
+        <a href="/" className='navbar-brand'>React with Contact App</a>
       </nav>
-      <UserList/>
     </Fragment>
     
   );
