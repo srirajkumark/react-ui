6b6bd7e001e045d76fde1c145a83e9c11bfca5f4
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-08-08 14:23:42 +0530

    Router Configuration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment} from 'react';
 import './App.css';
-import { BrowserRouter, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
+import NavBar from "./modules/layout/components/NavBar";
+import Home from "./modules/layout/components/Home";
 
 let App = () => {
   return (
@@ -8,7 +10,11 @@ let App = () => {
       <BrowserRouter>
           <NavBar/>
           <Switch>
-            
+            <Route exact={true} path={'/'} Component={Home}/>
+            <Route exact={true} path={'/employee/list'} Component={EmployeeList}/>
+            <Route exact={true} path={'/about'} Component={About}/>
+            <Route exact={true} path={'/users/login'} Component={UserLogin}/>
+            <Route exact={true} path={'/users/register'} Component={UserRegister}/>
           </Switch>
       </BrowserRouter>
     </Fragment>
