6fb6dd87de99fb418515e306f1444279980ea326
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-08-10 22:40:06 +0530

    Router Configuration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Fragment} from 'react';
 import './App.css';
-import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import NavBar from "./modules/layout/components/NavBar";
 import Home from "./modules/layout/components/Home";
+import EmployeeList from "./modules/employees/components/EmployeeList";
+import About from "./modules/layout/components/About";
+import UserLogin from "./modules/users/components/UserLogin";
+import UserRegister from "./modules/users/components/UserRegister";
+import PageNotFound from './modules/layout/components/PageNotFound';
+
 
 let App = () => {
   return (
     <Fragment>
       <BrowserRouter>
-          <NavBar/>
-          <Switch>
+          {/* <NavBar/> */}
+          <Routes>
             <Route exact={true} path={'/'} Component={Home}/>
             <Route exact={true} path={'/employee/list'} Component={EmployeeList}/>
             <Route exact={true} path={'/about'} Component={About}/>
             <Route exact={true} path={'/users/login'} Component={UserLogin}/>
             <Route exact={true} path={'/users/register'} Component={UserRegister}/>
-          </Switch>
+            <Route path={'*'} Component={PageNotFound}/>
+          </Routes>
       </BrowserRouter>
     </Fragment>
     
---
+++ src/modules/layout/components/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+let PageNotFound = () => {
+    return(
+        <React.Fragment>
+            <h2>Welcome to Page Not Found</h2>
+        </React.Fragment>
+    )
+};
+
+export default PageNotFound;
